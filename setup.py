7172eb0297d7e5502324490cf18225629e9ab431
Ben Abrams <babrams@certona.com>
2020-02-20 14:49:08 -0800

    Small organization refactor and added setup.py for dependency management

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='dst0gui',
+      version='0.1',
+      description="Web based GUI for Don't Starve Togther Server Management",
+      url='http://github.com/combatwombat16/dst-gui',
+      author='Ben Abrams',
+      author_email='combatwombat16@gmail.com',
+      license='MIT',
+      packages=['dst-gui'],
+      install_requires=[
+          'flask',
+          'react'
+      ],
+      zip_safe=False)
